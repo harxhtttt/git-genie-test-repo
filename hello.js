367dc89a9fdeb101ae2200a3bfc78dc67a68d3e7
harxhtttt <jhah0648@gmail.com>
2026-09-18 13:18:34 +0530

    Apply suggestion from @git-genie02[bot]
    Co-authored-by: git-genie02[bot] <330288276+git-genie02[bot]@users.noreply.github.com>

--- hello.js
+++ hello.js
@@ -9,7 +9,7 @@ function printGoodbye() {
 module.exports = { printHello, printGoodbye };
 
 function findDuplicates(arr1, arr2) {
-  const set2 = new Set(arr2);
+const set2 = new Set(arr2);
   return [...new Set(arr1.filter(item => set2.has(item)))];
   return duplicates;
 }
