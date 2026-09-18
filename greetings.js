c512916a5f678ae96f10ef777b42563b10f8c889
harxhtttt <jhah0648@gmail.com>
2026-09-18 13:14:31 +0530

    Add findDuplicates function to greetings.js

--- greetings.js
+++ greetings.js
@@ -7,3 +7,17 @@ function printGoodbye() {
 }
 
 module.exports = { printHello, printGoodbye };
+
+function findDuplicates(arr1, arr2) {
+  const duplicates = [];
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        duplicates.push(arr1[i]);
+      }
+    }
+  }
+  return duplicates;
+}
+
+module.exports = { printHello, printGoodbye, findDuplicates };
